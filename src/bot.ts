3b55c18dd34fc3832320c4c3769d285fe3de8009
nycolas.messias1 <nycolas.messias@sossego.com.br>
2021-08-24 23:12:03 -0300

    Create greet/help dialogs

--- src/bot.ts
+++ src/bot.ts
@@ -8,15 +8,23 @@ export class EchoBot extends ActivityHandler {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
+
+            if (context.activity.text.toLowerCase() === "help") {
+                const replyText = `I'm Bro Bot. My purpose is to suggest you some movies. Try something like 'best action' or 'browse categories'.`
+                await context.sendActivity(MessageFactory.text(replyText, replyText));
+                await next();
+            } else {
+                const replyText = `Echo: ${ context.activity.text }`;
+                await context.sendActivity(MessageFactory.text(replyText, replyText));
+                // By calling next() you ensure that the next BotHandler is run.
+                await next();
+            }
         });
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome!';
+            const welcomeText = 
+            `Oh, hello ${context.activity.from.name}! I'm Bro Bot. I will suggest you movies, try something like 'best dramas' or 'browse categories'.`;
             for (const member of membersAdded) {
                 if (member.id !== context.activity.recipient.id) {
                     await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
